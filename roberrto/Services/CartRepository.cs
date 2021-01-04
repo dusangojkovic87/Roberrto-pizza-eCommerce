b9b53aae8a1211bc7cdfe28c40b1b5316c159f41
Dusan Gojkovic <dusangojkovic087gmail.com>
2021-01-04 03:30:00 -0800

    cart item quantity sum dodat

--- roberrto/Services/CartRepository.cs
+++ roberrto/Services/CartRepository.cs
@@ -74,7 +74,6 @@ namespace roberrto.Services
             if (product != null)
             {
                 product.Quantity += 1;
-                product.Price *= 2;
                 _context.CartItems.Update(product);
                 _context.SaveChanges();
             }
@@ -87,7 +86,6 @@ namespace roberrto.Services
             if (product != null)
             {
                 product.Quantity -= 1;
-                product.Price /= 2;
                 if (product.Quantity == 0)
                 {
                     _context.CartItems.Remove(product);
